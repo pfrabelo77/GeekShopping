33c8949f23feeb63a797b3050263ddd500c5bf14
Paulo Fernando Rabelo de Almeida <pfrabelo77@gmail.com>
2024-12-31 19:04:28 -0300

    GeekShopping.Web Models, Interfaces, Utils

---
+++ GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException
+                    (
+                    $"Algo aconteceu de errado chamando a Api: " +
+                    $"{response.ReasonPhrase}");
+            }
+            else
+            {
+                var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                var jsonSerializeOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var dataAsJson = JsonSerializer.Deserialize<T>(dataAsString, jsonSerializeOptions);
+                return dataAsJson;
+
+            }
+        }
+    }
+}

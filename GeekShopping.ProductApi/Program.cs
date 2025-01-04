b5aec73e64f32805c7019c189348dd9edd6d326c
Paulo Fernando Rabelo de Almeida <pfrabelo77@gmail.com>
2025-01-04 14:35:15 -0300

    aula 124. 1119 Adicionando Autenticação ao Swagger (Open API)

--- GeekShopping.ProductApi/Program.cs
+++ GeekShopping.ProductApi/Program.cs
@@ -75,6 +75,7 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

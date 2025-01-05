879f1d4a1f07c433f2f6d5e6d6eaff07cae5dfbd
Paulo Fernando Rabelo de Almeida <pfrabelo77@gmail.com>
2025-01-05 20:57:42 -0300

    139. 1205 Ajustando o HomeController para Permitir Exibir Detalhes do Produto

--- GeekShopping.Web/Models/ProductModel.cs
+++ GeekShopping.Web/Models/ProductModel.cs
@@ -13,5 +13,27 @@
 
         public string ImageUrl { get; set; }
 
+        public string SubstringName() {
+            if (Name.Length < 24)
+            {
+                return Name;
+            }
+            else
+            {
+                return $"{ Name.Substring(0, 21)}...";
+            }
+        }
+        public string SubstringDescription()
+        {
+            if (Description.Length < 355)
+            {
+                return Description;
+            }
+            else
+            {
+                return $"{Description.Substring(0, 352)}...";
+            }
+        }
+
     }
 }

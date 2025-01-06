0f2e8291aec708e9afc898fb7bdbbfcd8720f377
Paulo Fernando Rabelo de Almeida <pfrabelo77@gmail.com>
2025-01-06 13:25:13 -0300

    49. 1306 Ajustando o DbContext e Aplicando às Migrations à Base de Dados

---
+++ GeekShopping.CartApi/Model/Context/MySQLServerContext.cs
@@ -0,0 +1,15 @@
+﻿using GeekShopping.CartAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartApi.Model.Context
+{
+    public partial class MySQLServerContext : DbContext
+    {
+        public MySQLServerContext(DbContextOptions<MySQLServerContext> options) : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+
+    }
+}

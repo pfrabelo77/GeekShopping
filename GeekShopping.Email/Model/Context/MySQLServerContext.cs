5c8a60fabff3178fd19765112c9b113d741be464
Paulo Fernando Rabelo de Almeida <pfrabelo77@gmail.com>
2025-01-11 17:42:31 -0300

    244. 2410 Implementando o Consumo de Mensagens Através de Direct Exchanges

---
+++ GeekShopping.Email/Model/Context/MySQLServerContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.Email.Model.Context
+{
+    public partial class MySQLServerContext : DbContext
+    {
+        public MySQLServerContext(DbContextOptions<MySQLServerContext> options) : base(options) {}
+
+        public DbSet<EmailLog> Emails { get; set; }
+
+    }
+}
